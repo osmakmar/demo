9e8c5edc4a72f22fb2266db3cd9096b39284db1c
Mario Osmakcic <osmakmar@students.zhaw.ch>
2023-05-27 09:46:21 +0200

    Test 2

--- src/main/java/ch/zhaw/springboot/demo/ScoreKeeper.java
+++ src/main/java/ch/zhaw/springboot/demo/ScoreKeeper.java
@@ -8,4 +8,20 @@ public class ScoreKeeper {
         scoreTeamB = 0;
     }
 
+    public String getScore() {
+        return String.format("%03d:%03d", scoreTeamA, scoreTeamB);
+    }
+
+    public void scoreTeamA1() {
+        scoreTeamA += 1;
+    }
+
+    public void scoreTeamA2() {
+        scoreTeamA += 2;
+    }
+
+    public void scoreTeamA3() {
+        scoreTeamA += 3;
+    }
+
 }
--- src/test/java/ch/zhaw/springboot/demo/ScoreKeeperTest.java
+++ src/test/java/ch/zhaw/springboot/demo/ScoreKeeperTest.java
@@ -9,9 +9,32 @@ public class ScoreKeeperTest {
     private ScoreKeeper scoreKeeper;
 
     @BeforeEach
-    public void testInitialScore() {
+    public void setUp() {
         scoreKeeper = new ScoreKeeper();
+    }
+
+    @Test
+    public void testInitialScore() {
         assertEquals("000:000", scoreKeeper.getScore());
     }
-    
+
+    @Test
+    public void testScoreTeamA1() {
+        scoreKeeper.scoreTeamA1();
+        assertEquals("001:000", scoreKeeper.getScore());
+    }
+
+    @Test
+    public void testScoreTeamA2() {
+        scoreKeeper.scoreTeamA2();
+        assertEquals("002:000", scoreKeeper.getScore());
+    }
+
+    @Test
+    public void testScoreTeamA3() {
+        scoreKeeper.scoreTeamA3();
+        assertEquals("003:000", scoreKeeper.getScore());
+    }
+
 }
+
