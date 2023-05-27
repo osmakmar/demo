b29b1e1d1ad68b07d7e0b33695c2ba45331dd7d5
Mario Osmakcic <osmakmar@students.zhaw.ch>
2023-05-27 10:19:38 +0200

    Test 1

--- src/main/java/ch/zhaw/springboot/demo/ScoreKeeper.java
+++ src/main/java/ch/zhaw/springboot/demo/ScoreKeeper.java
@@ -8,32 +8,5 @@ public class ScoreKeeper {
         scoreTeamB = 0;
     }
 
-    public String getScore() {
-        return String.format("%03d:%03d", scoreTeamA, scoreTeamB);
-    }
-
-    public void scoreTeamA1() {
-        scoreTeamA += 1;
-    }
-
-    public void scoreTeamA2() {
-        scoreTeamA += 2;
-    }
-
-    public void scoreTeamA3() {
-        scoreTeamA += 3;
-    }
-
-    public void scoreTeamB1() {
-        scoreTeamB += 1;
-    }
-
-    public void scoreTeamB2() {
-        scoreTeamB += 2;
-    }
-
-    public void scoreTeamB3() {
-        scoreTeamB += 3;
-    }
-
+   
 }
