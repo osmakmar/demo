c574111aa23eb223a28a06466ce536c9018e9651
Mario Osmakcic <osmakmar@students.zhaw.ch>
2023-05-27 09:15:22 +0200

    Test 1

---
+++ src/main/java/ch/zhaw/springboot/demo/ScoreKeeper.java
@@ -0,0 +1,11 @@
+package ch.zhaw.springboot.demo;
+public class ScoreKeeper {
+    private int scoreTeamA;
+    private int scoreTeamB;
+    
+    public ScoreKeeper() {
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+    }
+
+}
